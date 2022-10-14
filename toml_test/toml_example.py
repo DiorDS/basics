4d0804a80b720337c35598f46662e2f29898dbb8
Gorunov Ilya <phone.mas@yandex.ru>
2022-10-14 20:23:14 +0300

    modified:   toml_test/toml_example.py

--- toml_test/toml_example.py
+++ toml_test/toml_example.py
@@ -1,6 +1,11 @@
-import tomllib
+#import tomllib for python 3.11
+import pathlib
 
-with open("base_config.toml", mode="rb") as fp:
+import tomli as tomllib
+
+wp = pathlib.Path(__file__).parent
+
+with open(wp / "base_config.toml", mode="rb") as fp:
      config = tomllib.load(fp)
 
 print(config)
