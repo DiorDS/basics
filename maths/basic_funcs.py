523645bc6b7f21dad8a582c7a16a6a39840e3f0f
Gorunov Ilya <phone.mas@yandex.ru>
2022-10-14 20:47:48 +0300

    modified:   maths/basic_funcs.py

--- maths/basic_funcs.py
+++ maths/basic_funcs.py
@@ -2,6 +2,8 @@ from typing import Iterable
 
 __version__ = "v1.0.141022"
 __release_tag__ = "Beta for github"
+__author__ = "IGorunov"
+__all__ = ["avg"]
 
 def avg(_list: Iterable[int | float]) -> float | int:
     # Срнеднее арифметическое из списка
