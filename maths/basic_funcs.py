3220232cab429faa8b1a384094f87f4659ecdaf4
Gorunov Ilya <phone.mas@yandex.ru>
2022-10-14 20:54:02 +0300

    modified:   maths/basic_funcs.py

--- maths/basic_funcs.py
+++ maths/basic_funcs.py
@@ -1,9 +1,27 @@
+import math
 from typing import Iterable
 
-__version__ = "v1.0.141022"
+__version__ = "v1.1.141022"
 __release_tag__ = "Beta for github"
 __author__ = "IGorunov"
-__all__ = ["avg"]
+__all__ = ["avg", "median", "std"]
+
+def median(_list: Iterable[int | float])  -> float | int: 
+    # Медиана списка
+    n = len(_list)
+    mid = n // 2
+    if n % 2 == 1:
+        return sorted(_list)[mid]
+    else:
+        return avg(sorted(_list)[mid-1:][:2])
+
+def std(_list: Iterable[int | float])  -> float | int: 
+    # Среднеквадратичное списка
+    mu = avg(_list)
+    n = len(_list)
+    n = n-1 if n in range(1, 30) else n  
+    square_deviation = lambda x: (x - mu) ** 2 
+    return math.sqrt(sum(map(square_deviation, _list)) / n)
 
 def avg(_list: Iterable[int | float]) -> float | int:
     # Срнеднее арифметическое из списка
