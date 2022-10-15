57241c0c17a98714f588683de5ed7203bd9d3b90
Gorunov Ilya <phone.mas@yandex.ru>
2022-10-15 13:43:12 +0300

    modified:   runtime_note.md 	modified:   test.py 	deleted:    .github/workflows/codeql.yml

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from enum import Enum
 from classes.enumerates import Colors
-from maths.basic_funcs import avg
+from maths.basic_funcs import avg, median
 from toml_test.toml_example import config
 
 
 assert issubclass(Colors, Enum)
 assert avg([1,2,3,4]) == 2.5
 assert config == {"PY-ENV":{"secret_key": "36ed6ds67d#@$%6d7s6d//^RD&^%6%&*rfdsdsytf"}}
+assert median([1,2,3,4]) == 2.5
 
 
 print("All tests gets good results!")
