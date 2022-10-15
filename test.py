ebdcd34556b8080820530a12fb9032d5f9b6b546
Gorunov Ilya <phone.mas@yandex.ru>
2022-10-15 10:16:17 +0300

    modified:   test.py modified:   toml_test/toml_example.py

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from enum import Enum
 from classes.enumerates import Colors
 from maths.basic_funcs import avg
+from toml_test.toml_example import config
 
 
 assert issubclass(Colors, Enum)
 assert avg([1,2,3,4]) == 2.5
+assert config == {"PY-ENV":{"secret_key": "36ed6ds67d#@$%6d7s6d//^RD&^%6%&*rfdsdsytf"}}
 
 
 print("All tests gets good results!")
--- toml_test/toml_example.py
+++ toml_test/toml_example.py
@@ -8,4 +8,3 @@ wp = pathlib.Path(__file__).parent
 with open(wp / "base_config.toml", mode="rb") as fp:
      config = tomllib.load(fp)
 
-assert config == {"PY-ENV":{"secret_key": "36ed6ds67d#@$%6d7s6d//^RD&^%6%&*rfdsdsytf"}}
